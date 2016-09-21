697299cd69c246efa85429def4dcb84e07ac899f
Jeff Mery <jmery@chef.io>
2016-09-21 18:51:00 -0400

    Initial commit of broken config

--- recipes/client.rb
+++ recipes/client.rb
@@ -3,3 +3,11 @@
 # Recipe:: client
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+template '/etc/ssh/ssh_config' do
+  source 'ssh_config.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
--- recipes/server.rb
+++ recipes/server.rb
@@ -3,3 +3,10 @@
 # Recipe:: server
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+template '/etc/ssh/sshd_config' do
+  source 'sshd_config.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
